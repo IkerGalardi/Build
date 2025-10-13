f4bf7fbf171de47760a193ed348fc002f922f56e
IkerGalardi <contacto.ikergalardi@gmail.com>
2025-10-13 15:46:17 +0200

    projman: remove comments and unnecessary newlines in header

--- include/Build.h
+++ include/Build.h
@@ -5,10 +5,9 @@
 
 typedef enum
 {
-    BLD_EXECUTABLE,      /// Runnable program
-    BLD_STATIC_LIBRARY,  /// Static library
-    BLD_DYNAMIC_LIBRARY, /// Dynamically linked library
-    BLD_LIBRARY,         /// Dynamically linked and static library
+    BLD_EXECUTABLE,
+    BLD_STATIC_LIBRARY,
+    BLD_DYNAMIC_LIBRARY,
 } BldProjectType;
 
 typedef enum
@@ -31,50 +30,16 @@ typedef struct
     BldLanguage language;
 } BldProject;
 
-/**
- * @brief Create an executable project given a name and language.
- *
- * @param projectName: name of the project, will be used for the executable
- * @param language: name of the project, will be used for the executable
- * @return an executable handle.
- */
 BldProject *BldNewProject(char *projectName,
                           BldProjectType type,
                           BldLanguage language);
 
-/**
- * @brief Add sources to the project
- *
- * @param project: project
- * @param ...: variadic argument of strings containing the names of the files to be
- *             compiled. Last parameter must be NULL.
- */
-void BldAddSources(BldProject *project,
-                   ...);
+void BldAddSources(BldProject *project, ...);
 
-/**
- * @brief Add include paths for the project
- *
- * @param project: project
- * @param ...: variadic argument of strings containing the paths to be usable for
- *             includes.
- */
-void BldAddIncludePaths(BldProject *project,
-                        ...);
+void BldAddIncludePaths(BldProject *project, ...);
 
-/**
- * @brief Add dependencies to the project
- *
- * @param project: project
- * @param ...: variadic argument of strings containing the paths to be usable for
- *             includes.
- */
-void BldAddDependencies(BldProject *project,
-                        ...);
+void BldAddDependencies(BldProject *project, ...);
 
-/**
- * @brief Generate the project.
- */
 void BldGenerate(BldProject *defaultTarget);
 
 #endif // BUILD_H
