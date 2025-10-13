5f27cb5c71e9d7302c256e9f80fe24cf37fb0609
IkerGalardi <contacto.ikergalardi@gmail.com>
2025-10-13 12:20:48 +0200

    projman: implement project modification functions
    By implementing the NewProject AddSources and AddIncludePaths a
    project can now be described.

---
+++ src/Project.c
@@ -0,0 +1,78 @@
+#include "../include/Build.h"
+
+#include <stddef.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+BldProject projects[100];
+size_t projectIndex = 0;
+
+BldProject *BldNewProject(char *projectName,
+                          BldProjectType type,
+                          BldLanguage language)
+{
+    BldProject *project = &projects[projectIndex];
+    projectIndex++;
+
+    memset(project->projectName, 0, 64);
+    strncpy(project->projectName, projectName, 63);
+    project->type = type;
+    project->sources = NULL;
+    project->language = language;
+
+    return project;
+}
+
+void BldAddSources(BldProject *project,
+                   ...)
+{
+    va_list ap;
+    va_start(ap, project);
+
+    char *source = va_arg(ap, char *);
+    while (source != NULL) {
+        size_t sourceLength = strlen(source);
+        size_t projectLength = 0;
+        if (project->sources != NULL)
+            projectLength = strlen(project->sources);
+
+        project->sources = realloc(project->sources, sourceLength + projectLength + 2);
+        strncat(project->sources, source, sourceLength + projectLength + 1);
+        strncat(project->sources, " ", sourceLength + projectLength + 1);
+
+        source = va_arg(ap, char *);
+    }
+    va_end(ap);
+}
+
+void BldAddIncludePaths(BldProject *project,
+                        ...)
+{
+    va_list ap;
+    va_start(ap, project);
+
+    char *includePath = va_arg(ap, char *);
+    while (includePath != NULL) {
+        size_t includePathLength = strlen(includePath);
+        size_t projectLength = 0;
+        if (project->includePaths != NULL)
+            projectLength = strlen(project->includePaths);
+
+        project->includePaths = realloc(project->includePaths,
+                                        includePathLength + projectLength + 2);
+        strncat(project->includePaths,
+                includePath,
+                includePathLength + projectLength + 1);
+        strncat(project->includePaths, " ", includePathLength + projectLength + 1);
+
+        includePath = va_arg(ap, char *);
+    }
+    va_end(ap);
+}
+
+void BldGenerate(BldProject *defaultTarget)
+{
+
+}
