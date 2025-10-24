12fd86bcbd2624cebf3170e93ab43b1df3e4f7ea
IkerGalardi <contacto.ikergalardi@gmail.com>
2025-10-24 12:41:18 +0200

    test: add artifact naming test to linux
    Its important that the LD_LIBRARY_PATH variable is set in order to use
    the local version first, so that we test the development version
    instead of the installed one.

--- scripts/test_linux.sh
+++ scripts/test_linux.sh
@@ -1,5 +1,9 @@
 set -e
 
+cd test/ArtifactNames
+sh test_linux.sh
+cd ../..
+
 cd test/Defines
 sh test.sh
 cd ../..
---
+++ test/ArtifactNames/test_linux.sh
@@ -0,0 +1,34 @@
+echo "Artifact naming test"
+
+export LD_LIBRARY_PATH=./
+
+set -e
+
+cc -o Build Build.c -L../../ -lBuild -I../../include > /dev/null
+
+./Build > /dev/null
+
+make > /dev/null
+make libTestDLib.so > /dev/null
+make libTestSLib.a > /dev/null
+
+if [ -e TestExe ]
+then
+    echo " · Executable artifact:      PASS"
+else
+    echo " · Executable artifact:      FAIL"
+fi
+
+if [ -e libTestDLib.so ]
+then
+    echo " · Dynamic library artifact: PASS"
+else
+    echo " · Dynamic library artifact: FAIL"
+fi
+
+if [ -e libTestSLib.a ]
+then
+    echo " · Static library artifact:  PASS"
+else
+    echo " · Static library artifact:  FAIL"
+fi
