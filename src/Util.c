4a3c809c50f431b96c4df0cff4726af38c920357
IkerGalardi <contacto.ikergalardi@gmail.com>
2025-11-07 14:46:48 +0100

    util: add string array utility

---
+++ src/Util.c
@@ -0,0 +1,29 @@
+#include "Util.h"
+
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+UtilStringArray UtilCreateStringArray()
+{
+    UtilStringArray sa;
+    sa.stringCount = 0;
+    sa.data = NULL;
+
+    return sa;
+}
+
+void UtilAppendToStringArray(UtilStringArray *sa, char *string)
+{
+    sa->stringCount++;
+    sa->data = realloc(sa->data, sizeof(char*) * sa->stringCount);
+    assert(sa->data != NULL);
+
+    size_t stringLenght = strlen(string);
+    char *newString = malloc(stringLenght + 1);
+    strncpy(newString, string, stringLenght);
+    newString[stringLenght] = '\0';
+    assert(strcmp(newString, string) == 0);
+
+    sa->data[sa->stringCount - 1] = newString;
+}
