c19c4685374ef6262e616bcb678c9887fe159ecc
IkerGalardi <contacto.ikergalardi@gmail.com>
2025-12-10 10:16:22 +0100

    test: add test for the public header function

--- test/LocalDependency/Build.c
+++ test/LocalDependency/Build.c
@@ -3,7 +3,8 @@
 int main(int argc, char **argv)
 {
     BldProject *lib = BldNewProject("lib", BLD_STATIC_LIBRARY, BLD_LANGUAGE_DEFAULT);
-    BldAddSources(lib, "lib.c");
+    BldAddSources(lib, "library/lib.c");
+    BldAddPublicHeader(lib, "library/lib.h");
 
     BldProject *exe = BldNewProject("exe", BLD_EXECUTABLE, BLD_LANGUAGE_DEFAULT);
     BldAddSources(exe, "exe.c");
